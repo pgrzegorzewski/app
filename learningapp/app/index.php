9362eeda8f74e230d4371fbc29211d0caadf9a09
pgrzegorzewski <pawel.grzegorzewski@aptvision.com>
2018-08-20 21:20:14 +0200

    registration form part 1

--- learningapp/app/index.php
+++ learningapp/app/index.php
@@ -24,6 +24,7 @@
 	<div class="container-fluid">
     	<div class = "container section">   
     	 	<div class="form"><h1><b>Q</b>u¿zzy</h1></div>
+    	 	
         	<form action = "login.php" method = "post">
         	
             	<div class="form">Login:<br /><input type="text" name="login" /><br/></div>
@@ -38,6 +39,12 @@
         	      unset($_SESSION['error']);
         	  }
         	?>
+        	<div class="form">
+        		<br/><br />
+        		<h5>Nie posiadasz jeszcze konta?</h5>
+        		<br /><a href = "register.php"><b>Zarejestruj</b></a>
+        	</div>
+        	
     	</div>
     <div class="footer">
 		© 2018 PAWEŁ GRZEGORZEWSKI ALL RIGHTS RESERVED
