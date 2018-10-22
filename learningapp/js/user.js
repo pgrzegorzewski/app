f87a321c5b073d659e9b1c6f1c5aab1b647afb2a
pgrzegorzewski <pawel.grzegorzewski@aptvision.com>
2018-10-22 22:21:51 +0200

    fix to user class elements suggestion color (set style exchanged with css class add)

--- learningapp/js/user.js
+++ learningapp/js/user.js
@@ -9,12 +9,7 @@ function highlightUserClass()
 	var classElements = document.getElementsByClassName('class_' + classNumber);
 	for(i = 0; i < classElements.length; i++)
 	{	
-		if(classElements[i].getElementsByTagName('a')[0])
-		{
-			classElements[i].getElementsByTagName('a')[0].style.color = "#AFC2D1";
-		}
-		classElements[i].style.backgroundColor = "#4D658D";
-		classElements[i].style.color = "#AFC2D1";
+		classElements[i].classList.add('userClassElement');
 		
 	}
 }
